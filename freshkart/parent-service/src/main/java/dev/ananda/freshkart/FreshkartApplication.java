84b07b86a1f0b0d5efac9b634429ba62f47bcf2a
ananda <anandagowdan3@gmail.com>
2022-02-12 14:19:42 +0530

    Added parent service

---
+++ freshkart/parent-service/src/main/java/dev/ananda/freshkart/FreshkartApplication.java
@@ -0,0 +1,13 @@
+package dev.ananda.freshkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshkartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshkartApplication.class, args);
+	}
+
+}
